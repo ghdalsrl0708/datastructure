b6fa79103b30458e0ca970e0f9459f15ba7527c5
hjjunl <50603209+hjjunl@users.noreply.github.com>
2019-11-20 22:13:49 +0900

    This one can be second try to get 'ps -al'

--- HyunJunLeeVIRUS/execv.c
+++ HyunJunLeeVIRUS/execv.c
@@ -1,3 +1,6 @@
+//두번째 방법으로 생각하던 문제였지만 첫번째 방법으로 끝냈습니다.
+//컴퓨터내 프로세스를 execv함수를 이용해서 받아옵니다.
+//By using execv fuction you can get 'ps -al' results.
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -16,6 +19,6 @@ case 0:
 execv("/bin/ps", args); break;
 }
 if (pid > 0) { wait(&status); }
-//by using fopen, fprintf I'm trying to make result in txt file(list of ps -al)
+
 return 0;
 }
